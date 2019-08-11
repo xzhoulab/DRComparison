ec7ac8d3b6ca3890d89ecf3c2e13ac3062687269
Shiquan Sun <ssqsxf@126.com>
2019-08-11 17:19:57 -0400

    Add files via upload

--- parameter_settings/params_settings.R
+++ parameter_settings/params_settings.R
@@ -1,6 +1,6 @@
 ## Parameter Settings
 ## Modified by Shiquan Sun
-## Date: 2019-4-3 08:26:59
+## Date: 2019-6-11 17:28:17
 ## Normalization methods: log, lcpm, lsd, and vst
 params_settings <- function(method, num_clust=NULL){
 	
@@ -8,18 +8,34 @@ params_settings <- function(method, num_clust=NULL){
 		params <- list(rotate = "oblimin", filtering_method="nonzeros", norm_method="log")
 	}else if(method=="PCA"){
 		params <- list(filtering_method="nonzeros", norm_method="log")
+	}else if(method=="SIMLR"){
+		params <- list(num_clust = num_clust, clust_range=c(2:20), filtering_method="nonzeros", norm_method="log")
 	}else if(method=="ZINBWaVE"){
 		params <- list(num_core=20, doParallel=TRUE, X=NULL, V=NULL, filtering_method="nonzeros")
-	}else if(method=="PoissonNMF"){
-		params <- list(num_core=5, doParallel=TRUE, filtering_method="nonzeros")
 	}else if(method=="NMF"){
-		params <- list(num_core=5, doParallel=TRUE, filtering_method="nonzeros", norm_method="log")
+		params <- list(num_core=5, doParallel=TRUE, algorithm="lee", loss="mkl", filtering_method="nonzeros")
+	}else if(method=="nNMF"){
+		params <- list(num_core=5, doParallel=TRUE, algorithm="lee", loss="mse", filtering_method="nonzeros", norm_method="log")
 	}else if(method=="pCMF"){
 		params <- list(num_core=20, doParallel=TRUE, filtering_method="nonzeros")
 	}else if(method=="DiffusionMap"){
 		params <- list(filtering_method="nonzeros", norm_method="log")
 	}else if(method=="ICA"){
 		params <- list(filtering_method="nonzeros", norm_method="log")
+	}else if(method=="GLMPCA"){
+		params <- list(filtering_method="nonzeros", fam="nb")
+	}else if(method=="tSNE"){
+		params <- list(filtering_method="nonzeros", norm_method="log", perplexity = 30)
+	}else if(method=="Isomap"){
+		params <- list(filtering_method="nonzeros", norm_method="log", num_prop=0.1)
+	}else if(method=="LLE"){
+		params <- list(filtering_method="nonzeros", norm_method="log", num_prop=0.1)
+	}else if(method=="MSD"){
+		params <- list(filtering_method="nonzeros", norm_method="log", num_prop=0.1)
+	}else if(method=="LLLE"){
+		params <- list(filtering_method="nonzeros", norm_method="log", num_prop=0.1)
+	}else if(method=="LTSA"){
+		params <- list(filtering_method="nonzeros", norm_method="log", num_prop=0.3)
 	}else{
 		stop("no defined method!")
 	}# end fi
