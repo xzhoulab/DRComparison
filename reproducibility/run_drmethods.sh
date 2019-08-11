221d516ad32c8307755668c5c6d4b6aa324af350
Shiquan Sun <ssqsxf@126.com>
2019-08-11 17:27:25 -0400

    Update run_drmethods.sh

--- reproducibility/run_drmethods.sh
+++ reproducibility/run_drmethods.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=dr
 #SBATCH --mem=5000
 #SBATCH --partition=nomosix
-#SBATCH --array=1-20
+#SBATCH --array=1-240
 #SBATCH --output=~/scRNAseqDRComparison/code/out/dr%a.out
 #SBATCH --error=~/scRNAseqDRComparison/code/err/dr%a.err
 #SBATCH --workdir=~/scRNAseqDRComparison/code
@@ -14,7 +14,7 @@ bash
 let k=0
 
 for id in 1; do
-for ((im=4; im<=4; im++)); do
+for ((im=1; im<=12; im++)); do
 for ((ip=1; ip<=4; ip++)); do
 for ((irpt=1; irpt<=5; irpt++)); do
   let k=${k}+1
